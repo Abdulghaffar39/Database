d619a9705f47a4ea3b3a984af5bd8624699420f5
Abdulghaffar <abdulghaffar15a@outlook.com>
2025-07-20 07:25:24 -0700

    sign in page

---
+++ app.js
@@ -0,0 +1,29 @@
+let box_1 = document.getElementById('box_1');
+let C1_parent_1 = document.getElementById('C1_parent_1');
+let C1_parent_2 = document.getElementById('C1_parent_2');
+
+// let box_2 = document.getElementById('box_2');
+let C2_parent_1 = document.getElementById('C2_parent_1');
+let C2_parent_2 = document.getElementById('C2_parent_2');
+
+// box_1.style.height = '769px';
+
+// box_2.style.display = 'none';
+C2_parent_1.style.display = 'none';
+C2_parent_2.style.display = 'none';
+
+function sign_up(){
+    
+    
+    box_1.style.height = '0px';
+
+    C1_parent_1.style.display = 'none';
+    C1_parent_2.style.display = 'none';
+
+    C2_parent_1.style.display = 'block';
+    C2_parent_2.style.display = 'block';
+    console.log('Abdul Ghaffar Your Function is worked');
+    
+    
+}
+
