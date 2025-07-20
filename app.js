9e69a4b6f20eb05ba76a7971b4ae28b92c46f7b2
Abdulghaffar <abdulghaffar15a@outlook.com>
2025-07-20 09:10:18 -0700

    pendding

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let C1_parent_2 = document.getElementById('C1_parent_2');
 let C2_parent_1 = document.getElementById('C2_parent_1');
 let C2_parent_2 = document.getElementById('C2_parent_2');
 
-// box_1.style.height = '769px';
+box_1.style.height = '773px';   
+box_2.style.height = '0px';   
 
 // box_2.style.display = 'none';
 C2_parent_1.style.display = 'none';
@@ -16,12 +17,31 @@ function sign_up(){
     
     
     box_1.style.height = '0px';
+    box_2.style.height = '773px';
 
     C1_parent_1.style.display = 'none';
     C1_parent_2.style.display = 'none';
 
     C2_parent_1.style.display = 'block';
     C2_parent_2.style.display = 'block';
+    console.log('Abdul Ghaffar Your Function is worked');
+    
+    
+}
+
+
+
+function sign_in(){
+    
+    
+    box_1.style.height = '773px';
+    box_2.style.height = '0px';
+
+    C1_parent_1.style.display = 'block';
+    C1_parent_2.style.display = 'block';
+
+    C2_parent_1.style.display = 'none';
+    C2_parent_2.style.display = 'none';
     console.log('Abdul Ghaffar Your Function is worked');
     
     
